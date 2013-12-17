23423d11a8278179ab3224911ac88adb065202c3
Bertrand Gressier <bertrand.gressier@adeoservices.com>
2013-12-17 11:59:29 +0100

    feat(jivepi): catch error in main program

--- jivepi.js
+++ jivepi.js
@@ -3,32 +3,36 @@ var ledManager = require('./ledManager');
 
 
 function main() {
-    lib.getLastActivity().then(function (type) {
-        switch (type) {
-        case 'COMMENT' :
-        case 'REPLY':
-            ledManager.turnOn(1, 1, 0);
-            break;
 
-        case 'ACCOUNT':
-            ledManager.turnOn(1, 0, 1);
-            break;
+    try {
+        lib.getLastActivity().then(function (type) {
+            switch (type) {
+            case 'COMMENT' :
+            case 'REPLY':
+                ledManager.turnOn(1, 1, 0);
+                break;
 
-        case 'LIKE':
-            ledManager.turnOn(1, 1, 0);
-            break;
-        case 'JOIN':
-            ledManager.turnOn(0, 0, 1);
-            break;
-        case 'CREATE':
-            ledManager.turnOn(0, 1, 0);
-            break;
-        case 'UNKNOWN':
-            console.log('UNKNOWN type');
-            break;
-        }
-    });
+            case 'ACCOUNT':
+                ledManager.turnOn(1, 0, 1);
+                break;
 
+            case 'LIKE':
+                ledManager.turnOn(1, 1, 0);
+                break;
+            case 'JOIN':
+                ledManager.turnOn(0, 0, 1);
+                break;
+            case 'CREATE':
+                ledManager.turnOn(0, 1, 0);
+                break;
+            case 'UNKNOWN':
+                console.log('UNKNOWN type');
+                break;
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
     setTimeout(main, 10000);
 };
 

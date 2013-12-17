4031bba0cce6786f2d05f3af79cc026fec09c665
Bertrand Gressier <bertrand.gressier@adeoservices.com>
2013-12-17 01:51:46 +0100

    feat(jivepi): add jive event for turn on led

--- jivepi.js
+++ jivepi.js
@@ -8,14 +8,30 @@ rule.second = [0, 10, 20, 30, 40, 50];
 
 schedule.scheduleJob(rule, function () {
     lib.getLastActivity().then(function (type) {
-
         switch (type) {
         case 'COMMENT' :
+        case 'REPLY':
             ledManager.turnOn(1, 1, 0);
+            break;
+
+        case 'ACCOUNT':
+            ledManager.turnOn(1, 0, 1);
+            break;
+
+        case 'LIKE':
+            ledManager.turnOn(1, 1, 0);
+            break;
+        case 'JOIN':
+            ledManager.turnOn(0, 0, 1);
+            break;
+        case 'CREATE':
+            ledManager.turnOn(0, 1, 0);
             break;
         }
     });
 });
+
+
 
 
 
